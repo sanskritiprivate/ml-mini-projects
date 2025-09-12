6eaa55d1979e1d184ac866bff75f7ec5b964c8f9
sanskrititimseena <timseena.sanskriti@gmail.com>
2025-09-12 15:44:17 -0600

    done cleaning pima dataset for linear regression

--- regression/linear_regression.py
+++ regression/linear_regression.py
@@ -12,4 +12,18 @@ file_path = path +'/diabetes.csv'
 
 df = pd.read_csv(file_path)
 print(df.head())
-print(df.info())
+print(df.info())     # column names, data types, missing values
+print(df.describe())  # summary statistics
+print(df.isnull().sum())  # count missing values
+
+# There are no null values, however, there are a lot of 0's that need
+#  to be replaced by null values and taken care of.
+cols_with_invalid_zeros = ["Glucose", "BloodPressure", "SkinThickness", "BMI", "Insulin"]
+df[cols_with_invalid_zeros] = df[cols_with_invalid_zeros].replace(0, np.nan)
+
+# then we replace all the nan values with the median
+df.fillna(df.median(), inplace=True)
+
+# verify after cleaning
+print(df.isnull().sum())  # should be all zeros now
+print(df.head())
