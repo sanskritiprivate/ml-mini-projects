2110a013d33f7a77f80ea9b301a789d23926f215
sanskrititimseena <timseena.sanskriti@gmail.com>
2025-09-23 15:23:20 -0600

    testing

--- end_to_end_ml_pipeline/data_ingestion.py
+++ end_to_end_ml_pipeline/data_ingestion.py
@@ -3,8 +3,10 @@
 import boto3
 import pandas as pd
 
-s3 = boto3.client("s3")
-bucket_name = "my-data-bucket"
+s3 = boto3.client("s3", endpoint_url="http://localhost:4566")
+s3.create_bucket(Bucket="my-bucket")
 
 def load_new_csvs(prefix="incoming/"):
+    # list all objects under prefix
+    objs = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
     pass
