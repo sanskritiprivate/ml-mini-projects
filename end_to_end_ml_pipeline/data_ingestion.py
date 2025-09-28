47efea1687383e91dda1e1c6f4af4aae4d787079
sanskrititimseena <timseena.sanskriti@gmail.com>
2025-09-28 10:45:45 -0400

    working on batch ingestion

--- end_to_end_ml_pipeline/data_ingestion.py
+++ end_to_end_ml_pipeline/data_ingestion.py
@@ -8,5 +8,13 @@ s3.create_bucket(Bucket="my-bucket")
 
 def load_new_csvs(prefix="incoming/"):
     # list all objects under prefix
-    # objs = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+    if "contents" not in response:
+        print("No new files found.")
+        return []
+
+    dfs = []
+    for obj in response["contents"]:
+        pass
     pass
