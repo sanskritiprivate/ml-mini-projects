c5e7f409c41ce3e7a0004f4f427a0aa30246bf62
sanskrititimseena <timseena.sanskriti@gmail.com>
2025-09-29 01:17:42 -0400

    test for batch data ingestion complete

--- end_to_end_ml_pipeline/data_ingestion.py
+++ end_to_end_ml_pipeline/data_ingestion.py
@@ -3,7 +3,11 @@
 import boto3
 import pandas as pd
 
-s3 = boto3.client("s3", endpoint_url="http://localhost:4566")
+s3 = boto3.client(
+    "s3",
+    endpoint_url="http://localhost:4566",  # LocalStack endpoint
+    aws_access_key_id="test",
+    aws_secret_access_key="test")
 bucket_name = "my-bucket"
 s3.create_bucket(Bucket=bucket_name)
 
@@ -11,7 +15,7 @@ def load_new_csvs(prefix="incoming/"):
     # list all objects under prefix
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
 
-    if "contents" not in response:
+    if "Contents" not in response:
         print("No new files found.")
         return []
 
@@ -24,4 +28,4 @@ def load_new_csvs(prefix="incoming/"):
             file_obj = s3.get_object(Bucket=bucket_name, Key=key)
             df = pd.read_csv(file_obj["Body"])
             dfs.append((key, df))
-    return []
+    return dfs
--- end_to_end_ml_pipeline/test_data_ingestion.py
+++ end_to_end_ml_pipeline/test_data_ingestion.py
@@ -1,5 +1,24 @@
 import pandas as pd
 from io import StringIO
+import boto3
+from data_ingestion import load_new_csvs
+
+# SETUP
+
+bucket_name = "my-bucket"
+# this line creates an S3 client using boto3,
+# which is Python’s library for interacting with AWS services.
+# Normally, the client would connect to Amazon’s real S3 servers,
+# but by specifying endpoint_url="https://localhost:4566",
+# you’re telling it to connect to a LocalStack instance running
+# on your computer instead. LocalStack emulates AWS services locally,
+# so this setup lets you create buckets, upload files, and test
+# S3 operations without using a real AWS account or paying for cloud resources.
+s3 = boto3.client(
+    "s3",
+    endpoint_url="http://localhost:4566",  # LocalStack endpoint
+    aws_access_key_id="test",
+    aws_secret_access_key="test")
 
 # create a small dataframe
 df = pd.DataFrame({
@@ -18,3 +37,25 @@ csv_buffer = StringIO()
 # index = false means we're not including the df's
 # index column in the csv
 df.to_csv(csv_buffer, index=False)
+
+# upload to s3 under "incoming/"
+# s3.put_object(Bucket=bucket_name, Key="incoming/test_file.csv")
+s3.put_object(
+    Bucket=bucket_name,
+    Key="incoming/test_file.csv",
+    Body=csv_buffer.getvalue()
+)
+
+print("Uploaded sample CSV to incoming/")
+
+
+# TEST
+
+def test_load_new_csvs():
+    files = load_new_csvs()
+
+    for key, df in files:
+        print(f"\nFile: {key}")
+        print(df.head())
+
+test_load_new_csvs()
