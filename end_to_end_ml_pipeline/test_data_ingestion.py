21c00c6458d4891c2c2b1b4a97b59317f8654630
sanskrititimseena <timseena.sanskriti@gmail.com>
2025-09-29 01:02:54 -0400

    testing, added some helpful note comments

--- end_to_end_ml_pipeline/test_data_ingestion.py
+++ end_to_end_ml_pipeline/test_data_ingestion.py
@@ -7,3 +7,14 @@ df = pd.DataFrame({
     "value": [10,20,30,40]
 })
 
+# a buffer is a temporary storage area
+# in memory (RAM) where data can be written
+# before it's read or sent somewhere else
+# instead of writing the csv to a file on disk,
+# here, we are writing it to a memory buffer
+# an in-memory string object using StringIO
+
+csv_buffer = StringIO()
+# index = false means we're not including the df's
+# index column in the csv
+df.to_csv(csv_buffer, index=False)
